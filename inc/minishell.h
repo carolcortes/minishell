f9a9bd1b96d1229468f59491d82e03c17dd611a5
carolcortes <caroline.ocortes@gmail.com>
2025-08-06 21:59:36 +0100

    code review

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 17:05:32 by cade-oli          #+#    #+#             */
-/*   Updated: 2025/08/04 23:10:11 by cade-oli         ###   ########.fr       */
+/*   Updated: 2025/08/06 21:57:27 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 # include <sys/wait.h>
 
 # include "../libft/libft/libft.h"
-# include "../libft/ft_printf/ft_printf.h"
 # include "../libft/get_next_line/get_next_line.h"
 
 #endif
