51af420a7e636ee39981af20726717b6b66fe6d2
Zorug <cassiano_gross@hotmail.com>
2025-09-13 20:32:43 +0100

    echo pipe problem

--- src/execution/external.c
+++ src/execution/external.c
@@ -6,11 +6,11 @@
 /*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 16:15:24 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/09/13 16:59:04 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/09/13 18:57:19 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/minishell.h"
+/*#include "../../inc/minishell.h"
 
 // Mude o retorno para int:
 int execute_external(t_token **args, char **envp)
@@ -50,4 +50,62 @@ int execute_external(t_token **args, char **envp)
     free(path);
     free_argv(argv);
     return (WEXITSTATUS(status)); // ✅ Retorna status de saída
+}*/
+
+#include "../../inc/minishell.h"
+
+int execute_external(t_token **args, char **envp)
+{
+    pid_t pid;
+    int status;
+    char *path;
+    char **argv;
+
+    // Converter t_token** para char** para execve
+    argv = tokens_to_argv(args);
+    if (!argv)
+        return (1);
+    
+    // Encontrar o caminho completo do executável
+    path = find_command_path(argv[0], envp);
+    
+    if (!path)
+    {
+        printf("minishell: %s: command not found\n", argv[0]);
+        free_argv(argv);
+        return (127); // ✅ Comando não encontrado
+    }
+
+    pid = fork();
+    if (pid == 0) // Processo filho
+    {
+        execve(path, argv, envp);
+        // Se execve retornar, é erro
+        printf("minishell: %s: execution failed\n", argv[0]);
+        free(path);
+        free_argv(argv);
+        exit(126); // ✅ Erro de execução
+    }
+    else if (pid > 0) // Processo pai
+    {
+        waitpid(pid, &status, 0);
+    }
+    else // Erro no fork
+    {
+        perror("minishell: fork");
+        free(path);
+        free_argv(argv);
+        return (1); // ✅ Erro de fork
+    }
+
+    free(path);
+    free_argv(argv);
+    
+    // Retornar o status de saída do comando
+    if (WIFEXITED(status))
+        return (WEXITSTATUS(status)); // ✅ Status normal de saída
+    else if (WIFSIGNALED(status))
+        return (128 + WTERMSIG(status)); // ✅ Terminado por sinal
+    else
+        return (1); // ✅ Fallback
 }
--- src/execution/path.c
+++ src/execution/path.c
@@ -6,12 +6,12 @@
 /*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 17:43:07 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/09/11 17:43:09 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/09/13 19:00:05 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Em src/execution/path.c
-#include "../../inc/minishell.h"
+/*#include "../../inc/minishell.h"
 
 char *find_command_path(char *command, char **envp)
 {
@@ -39,6 +39,59 @@ char *find_command_path(char *command, char **envp)
     
     // Dividir PATH por ':'
     paths = ft_split(path_env, ':');
+    i = 0;
+    while (paths[i])
+    {
+        path = ft_strjoin(paths[i], "/");
+        path = ft_strjoin_free(path, command, 1);
+        
+        if (access(path, X_OK) == 0)
+        {
+            free_array(paths);
+            return (path);
+        }
+        
+        free(path);
+        i++;
+    }
+    
+    free_array(paths);
+    return (NULL);
+}*/
+
+#include "../../inc/minishell.h"
+
+char *find_command_path(char *command, char **envp)
+{
+    char *path;
+    char *path_env;
+    char **paths;
+    int i;
+    
+    // Se já é um caminho absoluto/relativo
+    if (access(command, X_OK) == 0)
+        return (ft_strdup(command));
+    
+    // Buscar variável PATH
+    path_env = NULL;
+    i = 0;
+    while (envp[i])
+    {
+        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+        {
+            path_env = envp[i] + 5;
+            break;
+        }
+        i++;
+    }
+    
+    if (!path_env)
+        return (NULL);
+    
+    paths = ft_split(path_env, ':');
+    if (!paths)
+        return (NULL);
+    
     i = 0;
     while (paths[i])
     {
