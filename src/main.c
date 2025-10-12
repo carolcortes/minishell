cec38c9c357f2b6b2213a6ed999f9e0aed8d1769
Zorug <cassiano_gross@hotmail.com>
2025-10-12 17:20:31 +0100

    caminho envp

--- src/main.c
+++ src/main.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 17:05:01 by cade-oli          #+#    #+#             */
-/*   Updated: 2025/10/07 22:10:56 by cade-oli         ###   ########.fr       */
+/*   Updated: 2025/10/12 17:12:46 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static void	execute_with_redirections(t_command *cmd, char **env,
-	t_shell *shell)
+//static void	execute_with_redirections(t_command *cmd, char **env,
+//	t_shell *shell)
+static void	execute_with_redirections(t_command *cmd, t_shell *shell)
 {
 	pid_t	pid;
 
@@ -21,7 +22,8 @@ static void	execute_with_redirections(t_command *cmd, char **env,
 	if (pid == 0)
 	{
 		setup_child_signals();
-		exit(handle_child_process_single(cmd, env));
+		//exit(handle_child_process_single(cmd, shell->envp));
+		exit(handle_child_process_single(cmd, shell));
 	}
 	else if (pid > 0)
 		handle_parent_process(pid, shell);
@@ -32,7 +34,8 @@ static void	execute_with_redirections(t_command *cmd, char **env,
 	}
 }
 
-static void	process_single_command(t_command *cmd, char **env, t_shell *shell)
+//static void	process_single_command(t_command *cmd, char **env, t_shell *shell)
+static void	process_single_command(t_command *cmd, t_shell *shell)
 {
 	if (cmd->argc == 0)
 	{
@@ -46,17 +49,22 @@ static void	process_single_command(t_command *cmd, char **env, t_shell *shell)
 		return ;
 	}
 	if (cmd->redir_count > 0)
-		execute_with_redirections(cmd, env, shell);
+		//execute_with_redirections(cmd, env, shell);
+		//execute_with_redirections(cmd, shell->envp, shell);
+		execute_with_redirections(cmd, shell);
 	else
 	{
 		if (is_builtin(cmd->args))
-			shell->last_status = exec_builtin(cmd->args, env);
+			//shell->last_status = exec_builtin(cmd->args, env);
+			shell->last_status = exec_builtin(cmd->args, shell->envp);
 		else
-			shell->last_status = execute_external(cmd->args, env);
+			//shell->last_status = execute_external(cmd->args, shell->envp);
+			shell->last_status = execute_external(cmd->args, shell);
 	}
 }
 
-static void	process_input_line(char *line, char **env, t_shell *shell)
+//static void	process_input_line(char *line, char **env, t_shell *shell)
+static void	process_input_line(char *line, t_shell *shell)
 {
 	t_token		*tokens;
 	t_command	*pipeline;
@@ -70,15 +78,20 @@ static void	process_input_line(char *line, char **env, t_shell *shell)
 	if (pipeline)
 	{
 		if (pipeline->next)
-			execute_pipeline(pipeline, env, shell);
+			//execute_pipeline(pipeline, env, shell);
+			//execute_pipeline(pipeline, shell->envp, shell);
+			execute_pipeline(pipeline, shell);
 		else
-			process_single_command(pipeline, env, shell);
+			//process_single_command(pipeline, env, shell);
+			//process_single_command(pipeline, shell->envp, shell);
+			process_single_command(pipeline, shell);
 		free_pipeline(pipeline);
 	}
 	free_tokens(tokens);
 }
 
-static void	main_loop(char **env, t_shell *shell)
+//static void	main_loop(char **env, t_shell *shell)
+static void	main_loop(t_shell *shell)
 {
 	char	*line;
 
@@ -90,23 +103,28 @@ static void	main_loop(char **env, t_shell *shell)
 		line = shell_read_line();
 		if (!line)
 			break ;
-		process_input_line(line, env, shell);
+		process_input_line(line, shell);
+		//process_input_line(line, shell->envp, shell);
+		//process_input_line(line, env, shell);
 	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**env;
+	//char	**env;
 	t_shell	shell;
 
 	(void)argc;
 	(void)argv;
-	env = dup_env(envp);
+	//env = dup_env(envp);
+	shell.envp = dup_env(envp);
 	shell.last_status = 0;
 	printbanner();
 	setup_signals();
-	main_loop(env, &shell);
-	free_env(env);
+	//main_loop(env, &shell);
+	main_loop(&shell);
+	//free_env(env);
+	free_env(shell.envp);
 	rl_clear_history();
 	return (EXIT_SUCCESS);
 }
