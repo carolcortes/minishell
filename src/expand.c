73b6c6329e308c1b0fa5d5fd41cc156a2f0a38fb
Cassiano Gross Schuler <cgross-s@c2r2s5.42porto.com>
2025-10-18 15:46:15 +0100

    issue 38 done

--- src/expand.c
+++ src/expand.c
@@ -6,13 +6,32 @@
 /*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 20:30:00 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/10/03 14:52:43 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/10/18 15:44:27 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static char	*handle_env_var(const char *str, int *i, char *result)
+static char *get_env_value(char *key, char **envp)
+{
+    int     i;
+    int     len;
+
+    if (!key || !envp)
+        return (NULL);
+    len = ft_strlen(key);
+    i = 0;
+    while (envp[i])
+    {
+        if (ft_strncmp(envp[i], key, len) == 0 && envp[i][len] == '=')
+            return (envp[i] + len + 1);
+        i++;
+    }
+    return (NULL);
+}
+
+//static char	*handle_env_var(const char *str, int *i, char *result)
+static char	*handle_env_var(const char *str, int *i, char *result, t_shell *shell)
 {
 	int		start;
 	char	*var_name;
@@ -26,7 +45,8 @@ static char	*handle_env_var(const char *str, int *i, char *result)
 	var_name = ft_substr(str, start, *i - start);
 	if (!var_name)
 		return (NULL);
-	var_value = getenv(var_name);
+	//var_value = getenv(var_name);
+	var_value = get_env_value(var_name, shell->envp);
 	free(var_name);
 	if (var_value)
 	{
@@ -69,7 +89,8 @@ char	*expand_variables(const char *str, t_shell *shell)
 			i += 2;
 		}
 		else if (str[i] == '$' && (ft_isalpha(str[i + 1]) || str[i + 1] == '_'))
-			result = handle_env_var(str, &i, result);
+			//result = handle_env_var(str, &i, result);
+			result = handle_env_var(str, &i, result, shell);
 		else
 			result = handle_literal_char(str, &i, result);
 		if (!result)
