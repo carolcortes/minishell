0829839ff71f466a8e3f5f66173e7fe8daf478e0
carolcortes <caroline.ocortes@gmail.com>
2025-10-19 00:22:50 +0100

    remove comments

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -3,29 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 22:28:42 by cade-oli          #+#    #+#             */
-/*   Updated: 2025/10/18 16:52:40 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/10/19 00:22:08 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-/*int	ft_cd(t_token **args, char **envp)
-{
-	(void)envp;
-	if (!args[1])
-		return (chdir(getenv("HOME")));
-	if (chdir(args[1]->value) != 0)
-	{
-		perror("cd");
-		return (1);
-	}
-	return (0);
-}*/
-
-//Para ficar igual ao bash (resolve erro):
 int	ft_cd(t_token **args, char **envp)
 {
 	(void)envp;
