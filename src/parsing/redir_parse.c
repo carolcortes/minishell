041c90d6b3eace741f4895b1cc63a3954d08e084
Cassiano Gross <54179576+Zorug@users.noreply.github.com>
2025-10-21 21:51:46 +0100

    54 heredoc expansion (#60)
    * direcionando variáveis
    
    * redirections corrections
    
    * redir working with errors
    
    * issue working
    
    * issue done with norm
    
    * implementing mods
    
    * issue working
    
    * issue done with norm
    
    * Revert "resolve conflicts"
    
    This reverts commit 50f758021080c4737a5ec9ff0e9078a036ed6c2b, reversing
    changes made to 1e44bdc881ee98f4f6529c2d208ae51bbdc96270.
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: Cassiano Gross Schuler <cgross-s@c1r2s5.42porto.com>
    Co-authored-by: carolcortes <caroline.ocortes@gmail.com>

--- src/parsing/redir_parse.c
+++ src/parsing/redir_parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redir_parse.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 09:48:48 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/10/20 16:46:56 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/10/20 23:12:26 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,29 +78,29 @@ static int	validate_redirection_syntax(t_command *cmd, int i)
 
 static int process_redirection_token(t_command *cmd, int i, t_shell *shell)
 {
-    int     type;
-    char    *target;
-    bool    allow_expand;
+	int     type;
+	char    *target;
+	bool    allow_expand;
 
-    if (!validate_redirection_syntax(cmd, i))
-        return (0);
-    type = cmd->args[i]->redir_type;
-    target = cmd->args[i + 1]->value;
-    if (type == 4) // heredoc
-    {
-        // 🔑 se delimitador veio entre aspas → NÃO expande
-        if (cmd->args[i + 1]->quoted)
-            allow_expand = false;
-        else
-            allow_expand = true;
+	if (!validate_redirection_syntax(cmd, i))
+		return (0);
+	type = cmd->args[i]->redir_type;
+	target = cmd->args[i + 1]->value;
+	if (type == 4) // heredoc
+	{
+		// 🔑 se delimitador veio entre aspas → NÃO expande
+		if (cmd->args[i + 1]->quoted)
+			allow_expand = false;
+		else
+			allow_expand = true;
 
-        if (!handle_heredoc_redirection(cmd, target, allow_expand, shell))
-            return (0);
-    }
-    else
-        add_redirection(cmd, type, target);
-    remove_args(cmd, i, 2);
-    return (1);
+		if (!handle_heredoc_redirection(cmd, target, allow_expand, shell))
+			return (0);
+	}
+	else
+		add_redirection(cmd, type, target);
+	remove_args(cmd, i, 2);
+	return (1);
 }
 
 
