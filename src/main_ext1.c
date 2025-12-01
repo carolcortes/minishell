d96efa50599fc3e239c99eb265c609364f439f51
Zorug <cassiano_gross@hotmail.com>
2025-12-01 21:32:21 +0000

    conflicts

--- src/main_ext1.c
+++ src/main_ext1.c
@@ -6,7 +6,7 @@
 /*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 17:32:55 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/12/01 20:07:31 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/12/01 21:27:57 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,6 @@ char	*shell_read_line(void)
 	line = read_multiline_pipe(line);
 	if (line && *line)
 		add_history(line);
-	return (line);
-}*/
-
-char	*shell_read_line(void)
-{
-	char	*line;
-
-	line = readline("minishell$> ");
-	if (!line)
-		return (NULL);
-
-	// ⬇️ Aqui está a correção do pipe no final
-	line = read_multiline_pipe(line);
-
 	return (line);
 }
 
